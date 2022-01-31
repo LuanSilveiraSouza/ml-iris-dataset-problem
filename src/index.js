7238d8e48e78b3aa8959aea4c284c4c42a8848ea
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2022-01-31 19:53:31 -0300

    feat: add train-test split

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import { loadIris } from 'scikitjs-node'
-import chalk from 'chalk'
+import { loadIris } from "scikitjs-node";
+import chalk from "chalk";
 
-const { log } = console
+const { log } = console;
 
-;(async function main() {
-  console.log(chalk.blue("Exploring the Iris Dataset"))
+(async function main() {
+  console.log(chalk.blue("Exploring the Iris Dataset"));
 
-  const irisDataset = await loadIris()
+  let irisDataset = await loadIris();
 
-  log(irisDataset)
+  irisDataset.head(5).print();
 
-  irisDataset.tail(5).print()
+  irisDataset.describe().print();
 
-  irisDataset.describe().print()
+  formatTargetColumn(irisDataset).groupby(["target"]).count().print();
 
-  irisDataset.groupby(["target"]).count().print()
-})()
+  const [train, test] = await trainTestSplit(irisDataset, 0.6)
+})();
+
+const formatTargetColumn = (dataset) => {
+  return dataset.astype({ column: "target", dtype: "string" }).replace("0", "setosa", { columns: ["target"] })
+    .replace("1", "versicolor", { columns: ["target"] })
+    .replace("2", "virginica", { columns: ["target"] })
+}
+
+const trainTestSplit = async (dataset, trainSize) => {
+  const dtCount = dataset.count().values[0]
+
+  const dt = await dataset.sample(dtCount)
+
+  const trainCount= trainSize * dtCount
+
+  return [dt.iloc({rows: [`0:${trainCount}`]}), dt.iloc({rows: [`${trainCount}:${dtCount}`]})]
+}
